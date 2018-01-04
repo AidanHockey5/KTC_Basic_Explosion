543c1c8edaefada302859d2cec72015d663c84b4
Aidan Lawrence <aidan@aidanlawrence.com>
2018-01-04 12:21:37 -0800

    Created explosion project

---
+++ Assets/Bomb.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public float boomTime = 5f;
+    public float power = 10f;
+    public float radius = 10f;
+    public GameObject explosionPtc;
+    public AudioClip explosionSound;
+	// Use this for initialization
+	void Start ()
+    {
+        Invoke("Explosion", boomTime);
+	}
+	
+    void Explosion()
+    {
+        Vector3 expPosition = transform.position;
+        Collider[] cols = Physics.OverlapSphere(expPosition, radius);
+        foreach (Collider hit in cols)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+                rb.AddExplosionForce(power, expPosition, radius, 3.0F);
+        }
+        GameObject ptc = Instantiate(explosionPtc, gameObject.transform.position, Quaternion.identity);
+        AudioSource.PlayClipAtPoint(explosionSound, gameObject.transform.position);
+        Destroy(ptc, 10f);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Launcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    public GameObject bomb;
+    Camera cam;
+    public float launchForce = 1000f;
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+	void Update ()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 screenPos = Input.mousePosition;
+            screenPos.z = 1;
+            Vector3 worldPos = cam.ScreenToWorldPoint(screenPos);
+            GameObject b = Instantiate(bomb, worldPos, cam.transform.rotation);
+            b.GetComponent<Rigidbody>().AddForce(b.transform.forward * launchForce);
+        }
+	}
+}
